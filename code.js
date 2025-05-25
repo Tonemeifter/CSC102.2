853befe56f370496978f406b5195293f2df1652e
Tonemeifter <rich.cardiel@gmail.com>
2025-05-25 02:10:54 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,8 @@
+function clickToPlay(){
+    document.getElementById("heading").innerHTML = "You Win!";
+
+}
+
+function change(){
+    document.getElementById("heading").innerHTML = "Welcome to the Game"
+}
